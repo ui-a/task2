5d3f5e733e73a6ecacfded51ffcefc904cfebddf
ui-a <135043176+ui-a@users.noreply.github.com>
2023-06-09 05:57:14 +0000

    task2-interruption2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,19 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
+
+
 
   def create
     @book = Book.new(book_params)
     if @book.save
       redirect_to book_path(@book.id)
     else
-      render :new
+      render :index
     end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -23,13 +23,13 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
